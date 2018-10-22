13f5e8bdf7e380c3f9579277d8c44e98732495a9
JCloudYu <j.cloud.yu@gmail.com>
2018-10-22 17:51:32 +0800

    Export types such as UInt64, Int64, UInt128, Int128 for developers

--- beson.js
+++ beson.js
@@ -7,10 +7,17 @@
 (()=>{
     'use strict';
 
-    const Serialize = require('./lib/serializer');
-    const Deserialize = require('./lib/deserializer');
+    const Serialize			= require('./lib/serializer');
+    const Deserialize		= require('./lib/deserializer');
+    const {UInt64, Int64}	= require( './types/uint64' );
+    const {UInt128, Int128}	= require( './types/uint64' );
     
     module.exports = {
+    	UInt64,
+		Int64,
+		UInt128,
+		Int128,
+		
         Serialize,
         Deserialize
     };
